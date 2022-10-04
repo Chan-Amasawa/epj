18fabcd4b0d1f2b91ad59c75b270bb5c23c703bc
Chan-Amasawa <chanamasawa@gmail.com>
2022-10-04 21:34:13 +0630

    second commit

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./style.scss";
 
 let items = [];
 let itemRow = document.querySelector(".item-row");
+let cartBtn = document.querySelector(".cart-btn");
 
 showLoaderUi();
 fetch("https://fakestoreapi.com/products")
@@ -14,7 +15,7 @@ fetch("https://fakestoreapi.com/products")
       let itemDiv = document.createElement("div");
       itemDiv.classList.add("col-md-6", "col-lg-4");
       itemDiv.innerHTML = `
-        <div class="card item-card mb-4">
+        <div class="card item-card">
           <div class="card-body d-flex flex-column">
             <div class="mb-3">
               <img src="${item.image}" class="item-img" alt="">
@@ -28,8 +29,8 @@ fetch("https://fakestoreapi.com/products")
 
             <div class="d-flex justify-content-between align-items-center mt-auto">
               <p class="fw-bold mb-0">$ <span>100</span></p>
-              <button class="btn btn-outline-primary">
-                <i class="bi bi-cart-plus"></i>
+              <button class="btn btn-outline-primary add-cart">
+                <i class="bi bi-cart-plus pe-none"></i> Add Cart
               </button>
             </div>
           </div>
@@ -40,3 +41,41 @@ fetch("https://fakestoreapi.com/products")
     console.log(json);
     removeLoaderUi();
   });
+
+itemRow.addEventListener("click", function (e) {
+  if (e.target.classList.contains("add-cart")) {
+    let btnParent = e.target.closest(".item-card");
+    let parentImg = btnParent.querySelector(".item-img");
+    console.log(parentImg);
+
+    let newImage = new Image();
+    newImage.src = parentImg.src;
+    newImage.style.position = "fixed";
+    newImage.style.height = 100 + "px";
+    newImage.style.zIndex = 2000;
+    newImage.style.transition = 1 + "s";
+    newImage.style.top = parentImg.getBoundingClientRect().top + "px";
+    newImage.style.left = parentImg.getBoundingClientRect().left + "px";
+    console.log(newImage);
+    document.body.append(newImage);
+
+    setTimeout(() => {
+      newImage.style.transform = "rotate(360deg)";
+      newImage.style.height = 0 + "px";
+      newImage.style.top =
+        cartBtn.querySelector(".bi").getBoundingClientRect().top + "px";
+      newImage.style.left =
+        cartBtn.querySelector(".bi").getBoundingClientRect().left + "px";
+    }, 1000);
+    setTimeout(() => {
+      cartBtn.classList.add("animate__tada");
+      cartBtn.addEventListener("animationend", () => {
+        cartBtn.classList.remove("animate__tada");
+      });
+    }, 800);
+  }
+});
+
+// window.addToCart = (event) => {
+//   console.log("add-to-cart", event.target);
+// };
